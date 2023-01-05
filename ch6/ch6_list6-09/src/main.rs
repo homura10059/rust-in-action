012188d2d03eabb88c3c801a1ecc3c9295e59bc7
homura <preasper0+github@gmail.com>
2023-01-05 19:50:49 +0900

    add

---
+++ ch6/ch6_list6-09/src/main.rs
@@ -0,0 +1,158 @@
+use graphics::math::{add, mul_scalar, Vec2d};
+use piston_window::*;
+use rand::prelude::*;
+use std::alloc::{GlobalAlloc, Layout, System};
+use std::time::Instant;
+
+struct ReportingAllocator;
+
+#[global_allocator]
+static ALLOCATOR: ReportingAllocator = ReportingAllocator;
+
+unsafe impl GlobalAlloc for ReportingAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let start = Instant::now();
+        let ptr = System.alloc(layout);
+        let end = Instant::now();
+        let time_taken = end - start;
+        let bytes_requested = layout.size();
+        eprintln!("{}\t{}", bytes_requested, time_taken.as_nanos());
+        ptr
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+    }
+
+    unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
+        System.alloc_zeroed(layout)
+    }
+
+    unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
+        System.realloc(ptr, layout, new_size)
+    }
+}
+
+struct Particle {
+    height: f64,
+    width: f64,
+    position: Vec2d<f64>,
+    velocity: Vec2d<f64>,
+    acceleration: Vec2d<f64>,
+    color: [f32; 4],
+}
+
+impl Particle {
+    fn new(world: &World) -> Particle {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0.0..world.width);
+        let y = world.height;
+        let x_velocity = 0.0;
+        let y_velocity = rng.gen_range(-2.0..0.0);
+        let x_acceleration = 0.0;
+        let y_acceleration = rng.gen_range(0.0..0.15);
+        Particle {
+            height: 4.0,
+            width: 4.0,
+            position: [x, y].into(),
+            velocity: [x_velocity, y_velocity].into(),
+            acceleration: [x_acceleration, y_acceleration].into(),
+            color: [1.0, 1.0, 1.0, 0.99],
+        }
+    }
+
+    fn update(&mut self) {
+        self.velocity = add(self.velocity, self.acceleration);
+        self.position = add(self.position, self.velocity);
+        self.acceleration = mul_scalar(self.acceleration, 0.7);
+        self.color[3] *= 0.995;
+    }
+}
+
+struct World {
+    current_turn: u64,
+    particles: Vec<Box<Particle>>,
+    height: f64,
+    width: f64,
+    rng: ThreadRng,
+}
+
+impl World {
+    fn new(width: f64, height: f64) -> World {
+        World {
+            current_turn: 0,
+            particles: vec![],
+            height,
+            width,
+            rng: thread_rng(),
+        }
+    }
+
+    fn add_shapes(&mut self, n: i32) {
+        for _ in 0..n.abs() {
+            let particle = Particle::new(&self);
+            let boxed_particle = Box::new(particle);
+            self.particles.push(boxed_particle);
+        }
+    }
+
+    fn remove_shapes(&mut self, n: i32) {
+        for _ in 0..n.abs() {
+            let to_delete = self
+                .particles
+                .iter()
+                .enumerate()
+                .find(|(_i, particle)| particle.color[3] < 0.02)
+                .map(|(i, _particle)| i);
+
+            match to_delete {
+                None => {
+                    self.particles.remove(0);
+                }
+                Some(i) => {
+                    self.particles.remove(i);
+                }
+            }
+        }
+    }
+
+    fn update(&mut self) {
+        let n = self.rng.gen_range(-3..=3);
+
+        if n > 0 {
+            self.add_shapes(n);
+        } else {
+            self.remove_shapes(n);
+        }
+
+        self.particles.shrink_to_fit();
+        for shape in &mut self.particles {
+            shape.update();
+        }
+        self.current_turn += 1;
+    }
+}
+
+fn main() {
+    let (width, height) = (1280.0, 960.0);
+    let mut window: PistonWindow = WindowSettings::new("particles", [width, height])
+        .exit_on_esc(true)
+        .build()
+        .expect("Cloud not create a window");
+
+    let mut world = World::new(width, height);
+    world.add_shapes(1000);
+
+    while let Some(event) = window.next() {
+        world.update();
+
+        window.draw_2d(&event, |ctx, renderer, _device| {
+            clear([0.15, 0.17, 0.17, 0.9], renderer);
+
+            for s in &mut world.particles {
+                let size = [s.position[0], s.position[1], s.width, s.height];
+                rectangle(s.color, size, ctx.transform, renderer);
+            }
+        });
+    }
+}
