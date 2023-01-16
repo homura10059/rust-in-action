fa19aea8caece29d1b9f2ecb262c701c7c57acb6
homura <preasper0+github@gmail.com>
2023-01-16 19:57:42 +0900

    add

--- ch8/ch8_stdlib/src/main.rs
+++ ch8/ch8_stdlib/src/main.rs
@@ -1,6 +1,18 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
-fn main() {
+fn main() -> std::io::Result<()> {
+    let host = "www.rustinaction.com:80";
+    let mut conn = TcpStream::connect(host)?;
+
+    conn.write_all(b"GET / HTTP/1.0")?;
+    conn.write_all(b"\r\n")?;
+
+    conn.write_all(b"HOST: www.rustinaction.com")?;
+    conn.write_all(b"\r\n\r\n")?;
+
+    std::io::copy(&mut conn, &mut std::io::stdout())?;
+
     println!("Hello, world!");
+    Ok(())
 }
